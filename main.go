dddc1afa3ade8822a66d6a499f8bcc196906f1a0
Sourabh0777 <sourabh.verma9770@gmail.com>
2025-04-16 09:08:08 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"example.com/rest-api/db"
+	"example.com/rest-api/routes"
+)
+
+func main() {
+	db.InitDB()
+	server := gin.Default()
+	routes.RegisterRoutes(server)
+	server.Run(":8080")
+}
