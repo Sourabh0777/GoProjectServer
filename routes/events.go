1ba4f97bc5f97de43ed08ee933e82bab16656f5a
Sourabh0777 <sourabh.verma9770@gmail.com>
2025-04-17 08:59:42 +0530

    API: JWT userId extraction completed

--- routes/events.go
+++ routes/events.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"example.com/rest-api/models"
+	"example.com/rest-api/utils"
 )
 
 func getEvents(context *gin.Context) {
@@ -17,12 +18,29 @@ func getEvents(context *gin.Context) {
 	context.JSON(http.StatusOK, events)
 }
 func createEvent(context *gin.Context) {
+	token := context.Request.Header.Get("Authorization")
+
+	if token == "" {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized."})
+		return
+	}
+
+	userId, err := utils.VerifyToken(token)
+
+	if err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized."})
+		return
+	}
+
 	var event models.Event
-	err := context.ShouldBindJSON(&event)
+	err = context.ShouldBindJSON(&event)
+
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse data"})
 		return
 	}
+	event.UserID = userId
+
 	err = event.Save()
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not create event"})
